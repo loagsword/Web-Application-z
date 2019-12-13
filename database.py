935626e597f645193ee22a9f88691bb562f804da
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-13 00:16:41 -0500

    Feature: added method for editing an instance

--- database.py
+++ database.py
@@ -15,3 +15,11 @@ def add_instance(model, **kwargs):
 def delete_instance(model, id):
     model.query.filter_by(id=id).delete()
     commit_changes()
+
+
+def edit_instance(model, id, **kwargs):
+    instance = model.query.filter_by(id=id).all()[0]
+    for attr, new_value in kwargs:
+        setattr(instance, attr, new_value)
+    commit_changes()
+
