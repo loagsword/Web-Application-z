8e6be1fc58fa868e6634ddd66beed2ba7286b164
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-13 00:16:09 -0500

    Feature: added method for creating instance

--- database.py
+++ database.py
@@ -4,3 +4,9 @@ from .models import db
 def get_all(model):
     data = model.query.all()
     return data
+
+
+def add_instance(model, **kwargs):
+    instance = model(**kwargs)
+    db.session.add(instance)
+    commit_changes()
