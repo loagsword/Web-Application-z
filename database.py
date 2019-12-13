1a0141e2554b7dcfe4ec77e73f707505c7c9b62b
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-13 00:16:27 -0500

    Feature: added method for deleting instance

--- database.py
+++ database.py
@@ -10,3 +10,8 @@ def add_instance(model, **kwargs):
     instance = model(**kwargs)
     db.session.add(instance)
     commit_changes()
+
+
+def delete_instance(model, id):
+    model.query.filter_by(id=id).delete()
+    commit_changes()
