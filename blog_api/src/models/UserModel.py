cdd0ae9be193ffbcea4288029bdd9e469e413ea9
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-20 13:21:35 -0500

    Feature: linked users to blogposts model

--- blog_api/src/models/UserModel.py
+++ blog_api/src/models/UserModel.py
@@ -3,6 +3,7 @@ from marshmallow import fields, Schema
 import datetime
 from . import db
 from ..app import bcrypt
+from .BlogpostModel import BlogpostModel
 
 
 class UserModel(db.Model):
@@ -19,6 +20,7 @@ class UserModel(db.Model):
     password = db.Column(db.String(128), nullable=True)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
+    blogposts = db.relationship('BlogpostModel', backref='users', lazy=True)
 
     # class constructor
     def __init__(self, data):
@@ -48,7 +50,7 @@ class UserModel(db.Model):
         db.session.commit()
 
     def __generate_hash(self, password):
-            return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
+        return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
 
     def check_hash(self, password):
         return bcrypt.check_password_hash(self.password, password)
@@ -63,3 +65,16 @@ class UserModel(db.Model):
 
     def __repr(self):
         return '<id {}>'.format(self.id)
+
+
+class UserSchema(Schema):
+    """
+    User Schema
+    """
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    password = fields.Str(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    modified_at = fields.DateTime(dump_only=True)
+    blogposts = fields.Nested(BlogpostSchema, many=True)
