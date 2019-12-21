53d54562a441031f2a2209a882afa9d61098a22a
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-21 02:43:05 -0500

    Fix: refactored file format

--- blog_api/src/views/UserView.py
+++ blog_api/src/views/UserView.py
@@ -42,41 +42,6 @@ def get_all():
     ser_users = user_schema.dump(users, many=True).data
     return custom_response(ser_users, 200)
 
-
-@user_api.route('/login', methods=['POST'])
-def login():
-    req_data = request.get_json()
-
-    data, error = user_schema.load(req_data, partial=True)
-    if error:
-        return custom_response(error, 400)
-
-    if not data.get('email') or not data.get('password'):
-        return custom_response({'error': 'you need email and password to sign in'}, 400)
-
-    user = UserModel.get_user_by_email(data.get('email'))
-    if not user:
-        return custom_response({'error': 'invalid credentials'}, 400)
-
-    if not user.check_hash(data.get('password')):
-        return custom_response({'error': 'invalid credentials'}, 400)
-
-    ser_data = user_schema.dump(user).data
-    token = Auth.generate_token(ser_data.get('id'))
-    return custom_response({'jwt_token': token}, 200)
-
-
-def custom_response(res, status_code):
-    """
-    Custom Response Function
-    """
-    return Response(
-        mimetype="application/json",
-        response=json.dumps(res),
-        status=status_code
-    )
-
-
 @user_api.route('/<int:user_id>', methods=['GET'])
 @Auth.auth_required
 def get_a_user(user_id):
@@ -129,6 +94,39 @@ def get_me():
     ser_user = user_schema.dump(user).data
     return custom_response(ser_user, 200)
 
+
+@user_api.route('/login', methods=['POST'])
+def login():
+    req_data = request.get_json()
+
+    data, error = user_schema.load(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+
+    if not data.get('email') or not data.get('password'):
+        return custom_response({'error': 'you need email and password to sign in'}, 400)
+
+    user = UserModel.get_user_by_email(data.get('email'))
+    if not user:
+        return custom_response({'error': 'invalid credentials'}, 400)
+
+    if not user.check_hash(data.get('password')):
+        return custom_response({'error': 'invalid credentials'}, 400)
+
+    ser_data = user_schema.dump(user).data
+    token = Auth.generate_token(ser_data.get('id'))
+    return custom_response({'jwt_token': token}, 200)
+
+
+def custom_response(res, status_code):
+    """
+    Custom Response Function
+    """
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
 
 
 
