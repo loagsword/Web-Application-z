3c8e2a657e3a619b97c6bd194eef9363333f3a43
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-21 01:51:40 -0500

    Fix: pep8 fixed indentation on file

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -1,4 +1,4 @@
-#/src/views/BlogpostView.py
+# /src/views/BlogpostView.py
 from flask import request, g, Blueprint, json, Response
 from ..shared.Authentication import Auth
 from ..models.BlogpostModel import BlogpostModel, BlogpostSchema
@@ -10,54 +10,54 @@ blogpost_schema = BlogpostSchema()
 @blogpost_api.route('/', methods=['POST'])
 @Auth.auth_required
 def create():
-  """
+    """
   Create Blogpost Function
   """
-  # app initialization
-  req_data = request.get_json()
-  req_data['owner_id'] = g.user.get('id')
-  data, error = blogpost_schema.load(req_data)
-  if error:
-    return custom_response(error, 400)
-  post = BlogpostModel(data)
-  post.save()
-  data = blogpost_schema.dump(post).data
-  return custom_response(data, 201)
+    # app initialization
+    req_data = request.get_json()
+    req_data['owner_id'] = g.user.get('id')
+    data, error = blogpost_schema.load(req_data)
+    if error:
+        return custom_response(error, 400)
+    post = BlogpostModel(data)
+    post.save()
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 201)
 
 
 # app initialization
 @blogpost_api.route('/', methods=['GET'])
 def get_all():
-  """
-  Get All Blogposts
-  """
-  posts = BlogpostModel.get_all_blogposts()
-  data = blogpost_schema.dump(posts, many=True).data
-  return custom_response(data, 200)
+    """
+    Get All Blogposts
+     """
+    posts = BlogpostModel.get_all_blogposts()
+    data = blogpost_schema.dump(posts, many=True).data
+    return custom_response(data, 200)
 
 
 @blogpost_api.route('/<int:blogpost_id>', methods=['GET'])
 def get_one(blogpost_id):
-  """
-  Get A Blogpost
-  """
-  post = BlogpostModel.get_one_blogpost(blogpost_id)
-  if not post:
-    return custom_response({'error': 'post not found'}, 404)
-  data = blogpost_schema.dump(post).data
-  return custom_response(data, 200)
+    """
+    Get A Blogpost
+    """
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 200)
 
 
 # app initialization
 def custom_response(res, status_code):
-  """
-  Custom Response Function
-  """
-  return Response(
-    mimetype="application/json",
-    response=json.dumps(res),
-    status=status_code
-  )
+    """
+    Custom Response Function
+    """
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
 
 
 @blogpost_api.route('/<int:blogpost_id>', methods=['PUT'])
@@ -82,18 +82,19 @@ def update(blogpost_id):
     data = blogpost_schema.dump(post).data
     return custom_response(data, 200)
 
+
 @blogpost_api.route('/<int:blogpost_id>', methods=['DELETE'])
 @Auth.auth_required
 def delete(blogpost_id):
-  """
+    """
   Delete A Blogpost
   """
-  post = BlogpostModel.get_one_blogpost(blogpost_id)
-  if not post:
-    return custom_response({'error': 'post not found'}, 404)
-  data = blogpost_schema.dump(post).data
-  if data.get('owner_id') != g.user.get('id'):
-    return custom_response({'error': 'permission denied'}, 400)
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    if data.get('owner_id') != g.user.get('id'):
+        return custom_response({'error': 'permission denied'}, 400)
 
-  post.delete()
-  return custom_response({'message': 'deleted'}, 204)
+    post.delete()
+    return custom_response({'message': 'deleted'}, 204)
