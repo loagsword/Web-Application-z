1cd3582e6323be3373298cacc70a631b88e02967
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-12-21 01:10:06 -0500

    Fix: pep8 fixed spacing on file

--- blog_api/src/views/UserView.py
+++ blog_api/src/views/UserView.py
@@ -35,9 +35,12 @@ def create():
 @user_api.route('/', methods=['GET'])
 @Auth.auth_required
 def get_all():
-  users = UserModel.get_all_users()
-  ser_users = user_schema.dump(users, many=True).data
-  return custom_response(ser_users, 200)
+    """
+    Get all users
+    """
+    users = UserModel.get_all_users()
+    ser_users = user_schema.dump(users, many=True).data
+    return custom_response(ser_users, 200)
 
 
 @user_api.route('/login', methods=['POST'])
@@ -45,7 +48,6 @@ def login():
     req_data = request.get_json()
 
     data, error = user_schema.load(req_data, partial=True)
-
     if error:
         return custom_response(error, 400)
 
@@ -53,7 +55,6 @@ def login():
         return custom_response({'error': 'you need email and password to sign in'}, 400)
 
     user = UserModel.get_user_by_email(data.get('email'))
-
     if not user:
         return custom_response({'error': 'invalid credentials'}, 400)
 
@@ -61,9 +62,7 @@ def login():
         return custom_response({'error': 'invalid credentials'}, 400)
 
     ser_data = user_schema.dump(user).data
-
     token = Auth.generate_token(ser_data.get('id'))
-
     return custom_response({'jwt_token': token}, 200)
 
 
